9f7553a7e48f0291d5497099e69806e3be9b227b
Joacosc04 <joakinsc04@gmail.com>
2022-09-13 19:00:35 -0300

    uno

---
+++ Spring12/src/main/java/com/example/NotificationService.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificationService {
+    public static void imprimirSaludo(){
+        System.out.println("Bienvenido!!");
+    }
+}
